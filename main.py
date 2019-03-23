becbdd55c740f38e9021e7dd67f79a91c30a4a64
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2019-03-23 12:09:28 +0300

    Minor tileset cleanup
     - renames
     - remove unnecessay fields and constants

--- main.py
+++ main.py
@@ -6,30 +6,28 @@ import math
 from random import choice
 
 TILESIZE = [15, 21, 30, 42, 60]
-TILE_FILE = 'data/tileset.png'
-TILE_SRC_SIZE = 30
 
 
 class Tileset:
-    def __init__(self, image, scale, src_size):
-        self.scale = scale
+    def __init__(self, size):
+        src_size = 30
+        image = pygame.image.load('data/tileset.png').convert()
         imageW, imageH = image.get_size()
         tilesW, tilesH = imageW // src_size, imageH // src_size
 
-        size = TILESIZE[scale]
-        self.tileset = []
-        self.img = pygame.Surface((tilesW * size, tilesH * size), flags=image.get_flags())
+        tileset = []
+        img = pygame.Surface((tilesW * size, tilesH * size), flags=image.get_flags())
         for i in range(tilesW):
-            self.tileset.append([])
+            tileset.append([])
             for j in range(tilesH):
                 clip = image.subsurface((i*src_size, j*src_size, src_size, src_size))
                 src = pygame.transform.scale(clip, (size, size))
-                self.img.blit(src, (i*size, j*size))
-                self.tileset[i].append(self.img.subsurface(i*size, j*size, size, size))
+                img.blit(src, (i*size, j*size))
+                tileset[i].append(img.subsurface(i*size, j*size, size, size))
 
-        self.stone = self.tileset[0][0]
-        self.floor = self.tileset[1][0]
-
+        self.stone = tileset[0][0]
+        self.floor = tileset[1][0]
+        self.size = size
 
 
 class WindowState:
@@ -42,8 +40,7 @@ class WindowState:
         self.font = pygame.font.Font(None, 36)
         self.clock = pygame.time.Clock()
 
-        img = pygame.image.load(TILE_FILE).convert()
-        self.tiles = [Tileset(img, i, TILE_SRC_SIZE) for i in range(len(TILESIZE))]
+        self.tileset_by_size = {size: Tileset(size) for size in TILESIZE}
         self.scale = 2
 
         self.map = load_map('map.map')
@@ -53,15 +50,17 @@ class WindowState:
         self.pos = (self.mapTW / 2 * TILESIZE[self.scale],
                     self.mapTH / 2 * TILESIZE[self.scale])
 
+    @property
+    def tileset(self):
+        return self.tileset_by_size[TILESIZE[self.scale]]
 
     def set_tile(self, i, j, pos):
         if self.map[i][j] == '.':
-            tile = self.tiles[self.scale].stone
+            tile = self.tileset.stone
         else:
-            tile = self.tiles[self.scale].floor
+            tile = self.tileset.floor
         self.background.blit(tile, pos)
 
-        
     def render(self):
         self.clock.tick()
         self.background.fill((200, 200, 200))
@@ -69,14 +68,15 @@ class WindowState:
         screenPW, screenPH = self.screen.get_size()
         cx, cy = self.pos   # in pixels
 
+        tile_size = self.tileset.size
         for i in range(self.mapTH):
-            y = i * TILESIZE[self.scale] - cy
+            y = i * tile_size - cy
             if y > screenPH: break
-            if y + TILESIZE[self.scale] <= 0: continue
+            if y + tile_size <= 0: continue
             for j in range(self.mapTW):
-                x = j * TILESIZE[self.scale] - cx
+                x = j * tile_size - cx
                 if x > screenPW: break
-                if x + TILESIZE[self.scale] <= 0: continue
+                if x + tile_size <= 0: continue
 
                 self.set_tile(i, j, (x, y))
 
@@ -90,8 +90,8 @@ class WindowState:
 
 
     def move_screen(self, dx, dy):
-        mW = len(self.map[0]) * TILESIZE[self.scale]
-        mH = len(self.map) * TILESIZE[self.scale]
+        mW = len(self.map[0]) * self.tileset.size
+        mH = len(self.map) * self.tileset.size
         sW, sH = self.screen.get_size()
         x, y = self.pos
         x += dx
@@ -114,20 +114,6 @@ class WindowState:
             return
         self.scale += ds
         self.move_screen(0, 0)
-
-
-
-def load_tile_table(filename, width, height):
-    tileset = pygame.image.load(filename).convert()
-    W, H = tileset.get_size();
-    tiles = []
-    for x in range (W // width):
-        tiles.append([])
-        for y in range(H // height):
-            rect = (x * width, y * height, width, height)
-            tiles[-1].append(tileset.subsurface(rect))
-    return tiles
-
 
 
 def generate_map(width, height):
