029ad9c72607f738d0d61254e0c527de0d66c4ee
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2019-03-23 13:54:43 +0300

    Cursor controlled by arrow keys; screen follows

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from copy import deepcopy
-import math
+import time
 from random import choice
+from dataclasses import dataclass
 
 import pygame
 import pygame.locals
@@ -38,6 +38,39 @@ class Tileset:
         self.cursor = image
 
 
+FIRST_KEY_PRESS_DELAY = 0.25
+REPEAT_KEY_PRESS_DELAY = 0.1
+# When a key is pressed and hold at moment 0,
+# key press events happen at the following times:
+#   0
+#   FIRST_KEY_PRESS_DELAY
+#   FIRST_KEY_PRESS_DELAY + REPEAT_KEY_PRESS_DELAY
+#   FIRST_KEY_PRESS_DELAY + REPEAT_KEY_PRESS_DELAY * 2
+#   ...
+
+@dataclass
+class KeyState:
+    cooldown: float = 0.0
+    num_presses: int = 0
+
+    def tick(self, dt, pressed):
+        '''Returns whether event should be fired this tick.'''
+        if not pressed:
+            self.cooldown = 0
+            self.num_presses = 0
+            return False
+
+        self.cooldown -= dt
+        if self.cooldown > 0:
+            return False
+        if self.num_presses == 0:
+            self.cooldown = FIRST_KEY_PRESS_DELAY
+        else:
+            self.cooldown = REPEAT_KEY_PRESS_DELAY
+        self.num_presses += 1
+        return True
+
+
 class WindowState:
     def __init__(self):
         # self.screen = pygame.display.set_mode((420, 300))
@@ -57,6 +90,11 @@ class WindowState:
         screen_size = Vec2(*self.screen.get_size()) / self.tileset.size
         self.screen_pos = (self.map_size - screen_size) * 0.5
         self.cursor_pos = Vec2(self.map_size.x // 2, self.map_size.y // 2)
+
+        self.up_key_state = KeyState()
+        self.down_key_state = KeyState()
+        self.left_key_state = KeyState()
+        self.right_key_state = KeyState()
 
     @property
     def tileset(self):
@@ -101,19 +139,44 @@ class WindowState:
 
         self.screen.blit(self.background, (0, 0))
 
-
-    def move_screen(self, dx, dy):
+    def clip_screen_to_map(self):
         screen_size = Vec2(*self.screen.get_size()) / self.tileset.size
-
-        self.screen_pos += Vec2(dx, dy) / self.tileset.size
         self.screen_pos.x = max(0, min(self.screen_pos.x, self.map_size.x - screen_size.x))
         self.screen_pos.y = max(0, min(self.screen_pos.y, self.map_size.y - screen_size.y))
 
     def rescale(self, ds):
         if self.scale + ds < 0 or self.scale + ds >= len(TILESIZE):
             return
+
+        factor = TILESIZE[self.scale + ds] / TILESIZE[self.scale]
         self.scale += ds
-        self.move_screen(0, 0)
+
+        # preserve relative location of the cursor on the screen
+        cursor_pos = self.cursor_pos + Vec2(0.5, 0.5)
+        self.screen_pos = cursor_pos - (cursor_pos - self.screen_pos) / factor
+
+        self.clip_screen_to_map()
+
+    def move_cursor(self, dx, dy):
+        self.cursor_pos += Vec2(dx, dy)
+        self.cursor_pos.x = max(0, min(self.cursor_pos.x, self.map_size.x - 1))
+        self.cursor_pos.y = max(0, min(self.cursor_pos.y, self.map_size.y - 1))
+
+        screen_size = Vec2(*self.screen.get_size()) / self.tileset.size
+
+        ALLOWED_GAP = 2
+        JUMP = 10
+
+        if self.cursor_pos.x + 0.5 < self.screen_pos.x + ALLOWED_GAP:
+            self.screen_pos.x -= JUMP
+        if self.cursor_pos.x - 0.5 > self.screen_pos.x + screen_size.x - ALLOWED_GAP:
+            self.screen_pos.x += JUMP
+        if self.cursor_pos.y + 0.5 < self.screen_pos.y + ALLOWED_GAP:
+            self.screen_pos.y -= JUMP
+        if self.cursor_pos.y - 0.5 > self.screen_pos.y + screen_size.y - ALLOWED_GAP:
+            self.screen_pos.y += JUMP
+
+        self.clip_screen_to_map()
 
 
 def generate_map(width, height):
@@ -133,6 +196,7 @@ def load_map(filename):
 def main():
     pygame.init()
     game = WindowState()
+    prev_time = time.time()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.locals.QUIT:
@@ -144,16 +208,19 @@ def main():
                     game.rescale(-1)
                 if event.key == pygame.K_PAGEDOWN:
                     game.rescale(1)
-        if pygame.key.get_pressed()[pygame.K_LEFT]:
-            game.move_screen(-5, 0)
-        if pygame.key.get_pressed()[pygame.K_RIGHT]:
-            game.move_screen(5, 0)
-        if pygame.key.get_pressed()[pygame.K_UP]:
-            game.move_screen(0, -5)
-        if pygame.key.get_pressed()[pygame.K_DOWN]:
-            game.move_screen(0, 5)
 
-        game.render()            
+        dt = time.time() - prev_time
+        prev_time += dt
+        if game.left_key_state.tick(dt, pygame.key.get_pressed()[pygame.K_LEFT]):
+            game.move_cursor(-1, 0)
+        if game.right_key_state.tick(dt, pygame.key.get_pressed()[pygame.K_RIGHT]):
+            game.move_cursor(1, 0)
+        if game.up_key_state.tick(dt, pygame.key.get_pressed()[pygame.K_UP]):
+            game.move_cursor(0, -1)
+        if game.down_key_state.tick(dt, pygame.key.get_pressed()[pygame.K_DOWN]):
+            game.move_cursor(0, 1)
+
+        game.render()
         pygame.display.update()
 
 
